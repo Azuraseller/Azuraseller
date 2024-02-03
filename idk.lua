dc037edc9db1b1623f56ed68f896266a7fbe2733
Azuraseller <157256868+Azuraseller@users.noreply.github.com>
2024-02-03 11:45:29 +0700

    Delete idk.lua

--- idk.lua
+++
@@ -1,3 +0,0 @@
-_G.Name = "Azu Hub" -- Put You Name Script Here
-_G.Logo = "https://tr.rbxcdn.com/30DAY-Avatar-EE90025D6FB06468CA27E16B5A3AA81D-Png/352/352/Avatar/Png/noFilter" -- Enter Your ID Image Here
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader"))()
