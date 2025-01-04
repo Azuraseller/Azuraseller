d1b304b3acedc7f50c23a761d4454b040800d8cb
Azuraseller <157256868+Azuraseller@users.noreply.github.com>
2025-01-04 18:36:47 +0700

    Update Heal.lua

--- Heal.lua
+++ Heal.lua
@@ -1,13 +1,17 @@
 local player = game.Players.LocalPlayer
-local character = player.Character
- 
+local character = player.Character or player.CharacterAdded:Wait()
+
 function GodMode()
+    -- Đặt MaxHealth thành giá trị rất lớn để máu vô hạn
+    character.Humanoid.MaxHealth = math.huge
+    character.Humanoid.Health = math.huge
+
+    -- Tự động hồi máu nếu máu giảm
     character.Humanoid:GetPropertyChangedSignal("Health"):Connect(function()
-        if character.Humanoid.Health < 100 then
-            character.Humanoid.Health = 100
+        if character.Humanoid.Health < math.huge then
+            character.Humanoid.Health = math.huge
         end
     end)
 end
- 
- 
+
 GodMode()
