57f5b445009f22ad34ca29336086c3738708c374
Azuraseller <157256868+Azuraseller@users.noreply.github.com>
2025-01-04 18:46:25 +0700

    Update Heal.lua

--- Heal.lua
+++ Heal.lua
@@ -2,14 +2,17 @@ local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
 
+local maxHealth = humanoid.MaxHealth -- Lấy giá trị máu tối đa
 local previousHealth = humanoid.Health -- Lưu lượng máu hiện tại làm mốc
 
 function GodMode()
     humanoid:GetPropertyChangedSignal("Health"):Connect(function()
-        if humanoid.Health < previousHealth then
-            humanoid.Health = previousHealth -- Hồi lại lượng máu trước đó
+        if previousHealth - humanoid.Health >= 100 then
+            humanoid.Health = maxHealth -- Hồi đầy máu
+            previousHealth = maxHealth -- Cập nhật mốc mới là đầy máu
+        else
+            previousHealth = humanoid.Health -- Cập nhật mốc hiện tại
         end
-        previousHealth = humanoid.Health -- Cập nhật mốc mới
     end)
 end
 
