4208f29032e3525ec631e29bdd51626889c3d484
Azuraseller <157256868+Azuraseller@users.noreply.github.com>
2025-01-04 18:19:50 +0700

    Update Heal.lua

--- Heal.lua
+++ Heal.lua
@@ -4,7 +4,7 @@ local character = player.Character
 function GodMode()
     character.Humanoid:GetPropertyChangedSignal("Health"):Connect(function()
         if character.Humanoid.Health < 283000 then
-            character.Humanoid.Health = 100
+            character.Humanoid.Health = 28300
         end
     end)
 end
