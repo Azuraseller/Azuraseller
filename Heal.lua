42bbec01226cd94f465c9c92ed96924c615e089d
Azuraseller <157256868+Azuraseller@users.noreply.github.com>
2025-03-11 20:37:59 +0700

    Update Heal.lua

--- Heal.lua
+++ Heal.lua
@@ -1,6 +1,6 @@
 -- Tải và thực thi cả ba script cùng lúc
 local scripts = {
-    "https://raw.githubusercontent.com/Azuraseller/Azuraseller/main/Aim.lua",
+    "https://rawscripts.net/raw/Dead-Rails-Alpha-Alpha-Aimbot-with-bind-30004",
     "https://raw.githubusercontent.com/Azuraseller/Azuraseller/main/Clip.lua",
     "https://raw.githubusercontent.com/Azuraseller/Azuraseller/main/Drail.lua"
 }
